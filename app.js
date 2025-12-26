d2b9aafb1757f11155f1a4233ca6ec528a7b46aa
Tejas Patle <tejaspatle77@gmail.com>
2025-12-26 11:09:48 +0530

    Backend Ecommerce

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', "http://localhost:4200");
+    res.header('Access-Control-Allow-Origin', "https://electronicecommerceshop.netlify.app");
     res.header('Access-Control-Allow-Headers', true);
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
